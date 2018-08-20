2980c0e7b76b717583d6c06ac1de6e956b6b4320
Lucas Duarte Oliveira <lwwcas@gmail.com>
2018-08-20 16:31:07 +0100

    Upload project

---
+++ src/Entities/helpers.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * Returns the current page.
+ *
+ * @return string
+ */
+function currentPage() : string
+{
+    return str_after(Request::path(), '/');
+}
+
+/**
+ * Returns the current page, independent of locale.
+ *
+ * @return string
+ */
+function currentPageWithoutLocale() : string
+{
+    return str_after(
+        str_start(str_replace(Request::root(), '', LaravelLocalization::getNonLocalizedURL()), '/'),
+        '/'
+    );
+}
+
+/**
+ * Verifies if the given url pages are the current page.
+ *
+ * @param array $urls
+ * @param bool $startsWith
+ * @return bool
+ */
+function isCurrentPageInArray(array $urls, bool $startsWith = false) : bool
+{
+    $page = currentPage();
+
+    if ($startsWith) {
+        foreach ($urls as $url) {
+            if ($url === $page) {
+                return true;
+            }
+        }
+    }
+
+    foreach ($urls as $url) {
+        if (starts_with($page, $url)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * Verifies if the given page is the current page the user is seeing.
+ *
+ * @param string|array $url
+ * @return boolean
+ */
+function isCurrentPage($url, bool $startsWith = false) : bool
+{
+    if (!$startsWith) {
+        return is_array($url) ? isCurrentPageInArray($url) : $url === currentPage();
+    }
+
+    return is_array($url) ? isCurrentPageInArray($url, true) :  starts_with(currentPage(), $url);
+}
+
+/**
+ * Switches the current locale.
+ *
+ * @param  string $locale
+ * @return string
+ */
+function switchLocale(string $locale) : string
+{
+    return '/' . $locale . '/' . currentPageWithoutLocale();
+}
+
+/**
+ * Provides all available locales.
+ *
+ * @return array
+ */
+function locales()
+{
+    return LaravelLocalization::getSupportedLocales();
+}
+
+/**
+ * Returns the current locale if the locale parameter is null. Otherwise,
+ * it sets the application locale.
+ *
+ * @param string $locale
+ * @return void
+ */
+function locale(?string $locale = null)
+{
+    return LaravelLocalization::setLocale($locale);
+}
+
+
+
+/**
+ *
+ */
+function urlRoute(?string $route = null, $parameters = [])
+{
+    $url = route($route, $parameters);
+    return str_replace(url('/').'/', "", $url);
+}
+
+/**
+ *
+ */
+function routeProcess($route)
+{
+    if(is_array($route)){
+        return urlRoute($route[0], isset($route[1]) ? $route[1] : null);
+    }
+
+    return urlRoute($route);
+}
+
+/**
+ * Creating and manipulating cache
+ *
+ * @param string $key
+ * @param int $minutes
+ * @param $callback
+ * @param function $anonFunc
+ *
+ * @return Illuminate\Support\Facades\Cache
+ */
+function cacheKey(string $key, int $minutes = 10080, $callback = null, $anonFunc = null)
+{
+    if (is_callable($anonFunc)) {
+        $callback = $anonFunc();
+    }
+
+    if (Cache::has($key)) {
+        return Cache::get($key);
+    }else{
+        return Cache::remember($key, $minutes, function() use ($callback) {
+            return $callback;
+        });
+    }
+
+    return null;
+}
+
+/**
+ * Remove cache by key
+ */
+function cacheKeyRemove(string $key)
+{
+    return Cache::forget($key);
+}
+
---
+++ src/Providers/HelpServiceProvider.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Modules\Help\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factory;
+
+class HelpServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerConfig();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            __DIR__.'/../Config/config.php' => config_path('help.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            __DIR__.'/../Config/config.php', 'help'
+        );
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
